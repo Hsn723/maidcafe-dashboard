ca1bec9b40cef0136bbf76eb878b989d3196515f
Natsumi Hoshino <me@kagehoshi.com>
2014-03-08 15:40:24 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+﻿var app = require('express')()
+	, server = require('http').createServer(app)
+	, io = require('socket.io').listen(server);
+
+server.listen(8000);
+
+// PostgreSQL client
+var pg = require('pg');
+var conString = "postgres://fuu:kenkajoutou@localhost/maidcafe";
+var client = new pg.Client(conString);
+client.connect();
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+// Socket.io
+io.sockets.on('connection', function(socket) {
+
+	socket.on('get_menu', function() {
+		client.query('SELECT * FROM get_menu()', function(err, result) {
+			if(err || result.rowCount === 0){
+				socket.emit('error', 'ERROR: getting menu');
+				return console.error('error getting menu', err);
+			}
+			socket.emit('menu_data', result.rows);
+			socket.emit('orders_heartbeat', {});
+		});
+	});
+	
+	socket.on('add_order', function(data) {
+		client.query('SELECT new_receipt($1)', data.table_no, function(err) {
+			if(err){
+				socket.emit('error', 'ERROR: creating receipt');
+				return console.error('error creating receipt', err);
+			}
+			socket.emit('success', 'DONE: creating receipt');
+		});
+		client.query('SELECT add_order($1, $2, $3)', [data.table_no, data.seat_no, data.menu_item], function(err) {
+			if(err){
+				socket.emit('error', 'ERROR: adding order');
+				return console.error('error adding order', err);
+			}
+			socket.emit('orders_heartbeat', {});
+			socket.emit('success', 'DONE: adding order');
+		});
+	});
+	
+	socket.on('get_unfilled_orders', function(data) {
+		client.query('SELECT * FROM get_unfilled_orders()', function(err, result) {
+			if(err){
+				socket.emit('error', 'ERROR: getting unfilled orders');
+				return console.error('error getting unfilled orders, err');
+			}
+			socket.emit('unfilled_orders_data', result.rows);
+		});
+	});
+	
+	socket.on('delete_order', function(data) {
+		client.query('SELECT delete_order($1)', [data], function(err) {
+			if(err){
+				socket.emit('error', 'ERROR: deleting order');
+				return console.error('error deleting order', err);
+			}
+			socket.emit('orders_heartbeat', {});
+		});
+	});
+	
+	socket.on('mark_fulfilled', function(data) {
+		client.query('SELECT mark_fulfilled($1)', [data], function(err) {
+			if(err){
+				socket.emit('error', 'ERROR: mark fulfilled');
+				return console.error('error marking fulfilled', err);
+			}
+			socket.emit('orders_heartbeat', {});
+		});
+	});
+	
+});
---
+++ public/js/socket-client.js
@@ -0,0 +1,66 @@
+﻿var socket = io.connect('http://kagehoshi.com:8000');
+
+var availableTables = ko.observableArray(['1','2','3','4','5','6','7','8','9','10']);
+var availableSeats = ko.observableArray(['1','2','3','4']);
+
+// Diagnostics
+socket.on('error', function(err) {
+	console.log(err);
+	$("#alert_container").addClass('alert-danger');
+	$("#alert_container").text(err);
+	$("#alert_container").fadeIn();
+	setInterval(function() {
+		$("#alert_container").fadeOut();
+		$("#alert_container").removeClass('alert-danger');
+		$("#alert_container").text('');
+	}, 1000);
+});
+socket.on('success', function(msg) {
+	$("#alert_container").addClass('alert-success');
+	$("#alert_container").text(msg);
+	$("#alert_container").fadeIn();
+	setInterval(function() {
+		$("#alert_container").fadeOut();
+		$("#alert_container").removeClass('alert-success');
+		$("#alert_container").text('');
+	}, 1000);
+});
+
+// Menu
+function get_menu(){
+	socket.emit('get_menu', {});
+}
+socket.on('menu_data', function(data) {
+	ko.applyBindings({
+		menu: data
+	}, document.getElementById("orders_container"));
+});
+
+// Add an order
+function add_order(menu_item){
+	var table_id = $("#table_selector option:selected").text();
+	var seat_id = $("#seat_selector option:selected").text();
+	socket.emit('add_order', {table_no : table_id, seat_no: seat_id, menu_item : menu_item.id});
+}
+
+var UnfilledOrdersViewModel = function(){
+	var self = this;
+	socket.on('orders_heartbeat', function() {
+		socket.emit('get_unfilled_orders', {});
+	});
+	socket.on('unfilled_orders_data', function(data){
+		self.unfilledOrders(data);
+	});
+	self.unfilledOrders = ko.observableArray([]);
+	socket.emit('get_unfilled_orders', {});
+};
+ko.applyBindings(new UnfilledOrdersViewModel(), document.getElementById('backlog_container'));
+
+// Delete an order
+function delete_order(data){
+	socket.emit('delete_order', data.order_no);
+}
+// Mark an order fulfilled
+function mark_fulfilled(data){
+	socket.emit('mark_fulfilled', data.order_no);
+}
